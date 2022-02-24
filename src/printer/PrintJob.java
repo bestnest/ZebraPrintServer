daa8d48adcf7c43e487d1e6a9aec24f87a89f5e0
Noah Broyles <noah@javamate.net>
2022-02-24 14:28:15 -0500

    Added image printing support

--- src/printer/PrintJob.java
+++ src/printer/PrintJob.java
@@ -1,27 +1,30 @@
 package printer;
 
 import java.util.Objects;
+import java.io.IOException;
 import com.zebra.sdk.comm.ConnectionException;
+import com.zebra.sdk.printer.ZebraPrinterLanguageUnknownException;
 
 
 public class PrintJob extends Thread {
 
-    private final ZebraLabelPrinter printer;
-    private final String printCode;
     private final String mode;
+    private final String printCode;
+    private final ZebraLabelPrinter printer;
 
     /***
-     * Creates a PrintJob with a specified mode. The available modes are "base64" and "raw".
+     * Creates a PrintJob with a specified mode. The available modes are "base64", "raw", and "image".
      * A job with "base64" mode will decode the labelCode string and then send the commands to the printer.
      * A job with "raw" mode will send the labelCode straight to the printer.
+     * A job in "image" mode will decode the printData and send it as an image to the printer.
      * @param printer The printer object to use
-     * @param labelCode the base64 encoded or plain text printer code to print
-     * @param mode The mode
+     * @param printData the base64 encoded or plain text printer code to print
+     * @param mode The mode. Options are "base64", "raw", and "image"
      */
-    public PrintJob(ZebraLabelPrinter printer, String labelCode, String mode) {
-        this.printer = printer;
-        this.printCode = labelCode;
+    public PrintJob(ZebraLabelPrinter printer, String printData, String mode) {
         this.mode = mode;
+        this.printer = printer;
+        this.printCode = printData;
     }
 
     @Override
@@ -32,8 +35,10 @@ public class PrintJob extends Thread {
                     this.printer.printBase64Content(this.printCode);
                 } else if (Objects.equals(this.mode, "raw")) {
                     this.printer.print(this.printCode);
+                } else if (Objects.equals(this.mode, "image")) {
+                    this.printer.printImage(this.printCode);
                 }
-            } catch (ConnectionException e) {
+            } catch (ConnectionException | ZebraPrinterLanguageUnknownException | IOException e) {
                 e.printStackTrace();
             }
         }
--- src/printer/ZebraLabelPrinter.java
+++ src/printer/ZebraLabelPrinter.java
@@ -1,9 +1,18 @@
 package printer;
 
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
 import com.zebra.sdk.comm.Connection;
 import java.nio.charset.StandardCharsets;
+import com.zebra.sdk.graphics.ZebraImageI;
+import com.zebra.sdk.printer.ZebraPrinter;
 import com.zebra.sdk.comm.ConnectionException;
 import org.apache.commons.codec.binary.Base64;
+import com.zebra.sdk.graphics.ZebraImageFactory;
+import com.zebra.sdk.printer.ZebraPrinterFactory;
+import com.zebra.sdk.printer.ZebraPrinterLanguageUnknownException;
 
 
 public class ZebraLabelPrinter {
@@ -27,6 +36,18 @@ public class ZebraLabelPrinter {
     public void printBase64Content(String base64encoded) throws ConnectionException {
         byte[] decoded = Base64.decodeBase64(base64encoded);
         this.print(decoded);
+    }
+
+    public void printImage(String base64ImageData) throws ZebraPrinterLanguageUnknownException, ConnectionException, IOException {
+        // Get a Zebra compatible image
+        InputStream rawImageData = new ByteArrayInputStream(Base64.decodeBase64(base64ImageData));
+        ZebraImageI image = ZebraImageFactory.getImage(rawImageData);
+
+        // Yeet it to the printer
+        this.connection.open();
+        ZebraPrinter imagePrinter = ZebraPrinterFactory.getInstance(this.connection);
+        imagePrinter.printImage(image, 0, 0, 0, 0, false);
+        this.connection.close();
     }
 
 }
